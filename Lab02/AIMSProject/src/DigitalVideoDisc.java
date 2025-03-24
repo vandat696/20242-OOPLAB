3aaf26eaecae87cf9108aa83c0a797ff6c9e858f
datthitran2005@gmail.com <datthitran2005@gmail.com>
2025-03-25 00:01:21 +0700

    section 15

--- Lab02/AIMSProject/src/DigitalVideoDisc.java
+++ Lab02/AIMSProject/src/DigitalVideoDisc.java
@@ -7,6 +7,10 @@ public class DigitalVideoDisc {
 	private int length;
 	private float cost;
 	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 	public String getTitle() {
 		return title;
 	}
